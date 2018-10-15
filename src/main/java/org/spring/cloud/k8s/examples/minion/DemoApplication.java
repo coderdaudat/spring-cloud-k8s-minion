b3926ed75c2f00d43bdba10b897a2a8967d12afa
coderdaudat <coderdaudat@gmail.com>
2018-10-15 10:59:26 +0700

    Initial import

---
+++ src/main/java/org/spring/cloud/k8s/examples/minion/DemoApplication.java
@@ -0,0 +1,12 @@
+package org.spring.cloud.k8s.examples.minion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
